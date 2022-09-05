c7401f0fabe02ac72728967234e13ba621cbe9db
dhaialsaho <dhaisaud711@gmail.com>
2022-09-05 20:14:44 +0300

    abtdhai

---
+++ classwork1/classwork1/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  classwork1
+//
+//  Created by Dhai AlSaho on 05/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        ZStack{
+            Image("Unknown")
+                .resizable()
+                .scaledToFit()
+            VStack {
+                Text("Hi, my name is Dhai")
+                Text("I'm 19 years old")
+                Text("I'm taking a coded course")
+                
+                HStack{
+                  Image(systemName: "moon")
+                        .foregroundColor(.white)
+                    Spacer()
+                    Image(systemName: "sparkle")
+                          .foregroundColor(.white)
+                    Spacer()
+                    Image(systemName: "cloud")
+                          .foregroundColor(.white)
+                }
+                    .padding()
+            }
+                
+        
+        }
+
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classwork1/classwork1/classwork1App.swift
@@ -0,0 +1,17 @@
+//
+//  classwork1App.swift
+//  classwork1
+//
+//  Created by Dhai AlSaho on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct classwork1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
